63d61f5fb8b86b3d181a844dbbef025a97eb559a
kaihuasun <no4long@gmail.com>
2020-08-17 20:19:02 +0800

    add lesson9

--- lesson9/substrate-node-template/pallets/template/src/lib.rs
+++ lesson9/substrate-node-template/pallets/template/src/lib.rs
@@ -9,8 +9,17 @@
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
-use frame_support::{debug, decl_module, decl_storage, decl_event, decl_error, dispatch};
-use frame_system::{self as system, ensure_signed};
+use frame_support::{debug, decl_module, decl_storage, decl_event, decl_error};
+use frame_system::{self as system,
+	offchain::{
+		AppCrypto, CreateSignedTransaction
+	},
+};
+use sp_runtime::{offchain as rt_offchain, offchain::storage::StorageValueRef};
+use sp_core::crypto::KeyTypeId;
+use sp_std::prelude::*;
+use sp_std::str;
+use alt_serde::{Deserialize, Deserializer};
 
 #[cfg(test)]
 mod mock;
@@ -18,11 +27,73 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-/// The pallet's configuration trait.
-pub trait Trait: system::Trait {
-	// Add other types and constants required to configure this pallet.
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+pub const ETH_PRICE_COINCAP_API: &[u8] = b"https://api.coincap.io/v2/assets/ethereum";
+pub const ETH_PRICE_BITFINEX_API: &[u8] = b"https://api.bitfinex.com/v1/pubticker/ethusd";
 
-	/// The overarching event type.
+#[serde(crate = "alt_serde")]
+#[derive(Debug, Deserialize)]
+pub struct CoinCap {
+	data: CoinCapData
+}
+
+#[serde(crate = "alt_serde")]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CoinCapData {
+	#[serde(deserialize_with = "de_string_to_f64")]
+	price_usd: f64
+}
+
+#[serde(crate = "alt_serde")]
+#[derive(Debug, Deserialize)]
+pub struct Bitfinex {
+	#[serde(deserialize_with = "de_string_to_f64")]
+	last_price: f64
+}
+
+fn de_string_to_f64<'der, D>(der: D) -> Result<f64, D::Error> where D: Deserializer<'der> {
+	let s: &str = Deserialize::deserialize(der)?;
+	Ok(s.parse::<f64>().unwrap())
+}
+
+pub mod crypto {
+	use crate::KEY_TYPE;
+	use sp_core::sr25519::Signature as Sr25519Signature;
+
+	use sp_runtime::{
+		app_crypto::{app_crypto, sr25519},
+		traits::Verify,
+		MultiSignature, MultiSigner,
+	};
+
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+	// implemented for ocw-runtime
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+
+	// implemented for mock runtime in test
+	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
+		for TestAuthId
+	{
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+}
+
+
+/// This is the pallet's configuration trait
+pub trait Trait: system::Trait + CreateSignedTransaction<Call<Self>>  {
+	/// The identifier type for an offchain worker.
+	type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+	/// The overarching dispatch call type.
+	type Call: From<Call<Self>>;
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
@@ -32,10 +103,6 @@ decl_storage! {
 	// storage items are isolated from other pallets.
 	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
 	}
 }
 
@@ -52,10 +119,10 @@ decl_event!(
 // The pallet's errors
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
+		ResponseParseError,
+		ResponseCharsetError,
+		HttpError,
+		AlreadyFetched
 	}
 }
 
@@ -72,25 +139,116 @@ decl_module! {
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
-		#[weight = 10_000]
-		pub fn save_number(origin, number: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
-
-			/*******
-			 * 学员们在这里追加逻辑
-			 *******/
-
-			Ok(())
-		}
-
-		fn offchain_worker(block_number: T::BlockNumber) {
+		fn offchain_worker(_block_number: T::BlockNumber) {
 			debug::info!("Entering off-chain workers");
 
 			/*******
 			 * 学员们在这里追加逻辑
 			 *******/
+			 let price_result = Self::fetch_eth_price();
+			 let result = Self::store_eth_price(price_result);
+			 if let Err(e) = result {
+				debug::error!("Error: {:?}", e);
+			}
+		}
+	}
+}
+
+impl<T: Trait> Module<T> {
+
+	fn store_eth_price(price_result: Result<u64, Error::<T>>) -> Result<(), Error<T>> {
+		let eth_prices = StorageValueRef::persistent(b"offchain-worker::eth-prices");
+		let eth_prices_lock = StorageValueRef::persistent(b"offchain-worker::eth-prices-lock");
+		let res: Result<Result<bool, bool>, Error<T>> = eth_prices_lock.mutate(|s: Option<Option<bool>>| {
+			match s {
+				// lock is never set or set to false
+				None | Some(Some(false)) => Ok(true),
+				// lock is set to true
+				_ => Err(<Error<T>>::AlreadyFetched),
+			}
+		});
+
+		if let Ok(Ok(true)) = res {
+			let mut prices: Vec::<u64> = Vec::<u64>::new();
+			if let Some(Some(fetched_prices)) = eth_prices.get::<Vec<u64>>() {
+				prices = fetched_prices;
+			}
+			match price_result {
+				Ok(price) => {
+					prices.push(price);
+					debug::info!("current price list: {:?}", prices);
+					eth_prices.set(&prices);
+					eth_prices_lock.set(&false);
+				}
+				Err(err) => {
+					eth_prices_lock.set(&false);
+					return Err(err);
+				}
+			}
 		}
 
+		Ok(())
+	}
+
+	fn fetch_eth_price() -> Result<u64, Error::<T>> {
+		let coincap_result = Self::fetch_data_from_api(&ETH_PRICE_COINCAP_API);
+		let bitfinex_result = Self::fetch_data_from_api(&ETH_PRICE_BITFINEX_API);
+		let coincap_price;
+		let bitfinex_price;
+		match coincap_result {
+			Ok(coincap_bytes) => {
+				let coincap_str = str::from_utf8(&coincap_bytes)
+					.map_err(|_| Error::<T>::ResponseCharsetError)?;
+				let coincap = serde_json::from_str::<CoinCap>(coincap_str)
+					.map_err(|_| Error::<T>::ResponseParseError)?;
+				debug::info!("coincap object: {:?}", coincap);
+				coincap_price = coincap.data.price_usd as u64;
+			}
+			Err(err) => {
+				return Err(err);
+			}
+		}
+		match bitfinex_result {
+			Ok(bitfinex_bytes) => {
+				let bitfinex_str = str::from_utf8(&bitfinex_bytes)
+					.map_err(|_| Error::<T>::ResponseCharsetError)?;
+				let bitfinex = serde_json::from_str::<Bitfinex>(bitfinex_str)
+					.map_err(|_| Error::<T>::ResponseParseError)?;
+				debug::info!("bitfinex object: {:?}", bitfinex);
+				bitfinex_price = bitfinex.last_price as u64;
+			}
+			Err(err) => {
+				return Err(err);
+			}
+		}
+		let price = (coincap_price + bitfinex_price) / 2;
+		Ok(price)
+	}
+
+	fn fetch_data_from_api(url_bytes: &[u8]) -> Result<Vec<u8>, Error<T>> {
+
+		let remote_url = str::from_utf8(url_bytes)
+			.map_err(|_| <Error<T>>::HttpError)?;
+		let request = rt_offchain::http::Request::get(remote_url);
+
+		let timeout = sp_io::offchain::timestamp().add(rt_offchain::Duration::from_millis(3000));
+
+		let pending = request
+			// .add_header("Content-Type", str::from_utf8(b"application/json;charset=utf-8")
+			.deadline(timeout)
+			.send()
+			.map_err(|_| <Error<T>>::HttpError)?;
+
+		let response = pending
+			.try_wait(timeout)
+			.map_err(|_| Error::<T>::HttpError)?
+			.map_err(|_| Error::<T>::HttpError)?;
+
+		if response.code != 200 {
+			debug::error!("Unexpected http status code: {}", response.code);
+			return Err(<Error<T>>::HttpError);
+		}
+
+		Ok(response.body().collect::<Vec<u8>>())
 	}
 }
--- lesson9/substrate-node-template/pallets/template/src/tests.rs
+++ lesson9/substrate-node-template/pallets/template/src/tests.rs
@@ -1,18 +1,21 @@
 // Tests to be written here
 
 use crate::{Error, mock::*};
-use frame_support::{assert_ok, assert_noop};
+use frame_support::{assert_ok};
 
 #[test]
-fn test_onchain() {
-	new_test_ext().execute_with(|| {
-		// Test onchain logic here
+fn test_offchain() {
+	let (mut t, _pool_state, _offchain_state) = ExtBuilder::build();
+	t.execute_with(|| {
+		assert_ok!(TemplateModule::store_eth_price(Ok(245)));
 	});
 }
 
 #[test]
-fn test_offchain() {
-	new_test_ext().execute_with(|| {
-		// Test offchain worker here
+fn test_offchain_error() {
+	let (mut t, _pool_state, _offchain_state) = ExtBuilder::build();
+	t.execute_with(|| {
+		let r = TemplateModule::store_eth_price(Err(Error::<Test>::HttpError));
+		assert!(r.is_err());
 	});
 }
